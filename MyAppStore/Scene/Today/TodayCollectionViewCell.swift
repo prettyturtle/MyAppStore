98420f01e31eeae4b4c8a02b4125cd27d8c2ddcc
prettyturtle <unifconti@gmail.com>
2021-12-18 01:27:12 +0900

    feat: TodayCollectionView, TodayCollectionViewCell

---
+++ MyAppStore/Scene/Today/TodayCollectionViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  TodayCollectionViewCell.swift
+//  MyAppStore
+//
+//  Created by yc on 2021/12/18.
+//
+
+import UIKit
+import SnapKit
+
+class TodayCollectionViewCell: UICollectionViewCell {
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 24.0, weight: .bold)
+        
+        return label
+    }()
+    
+    private lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.font = .systemFont(ofSize: 14.0, weight: .bold)
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        return imageView
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+        titleLabel.text = "title"
+        subTitleLabel.text = "subTitle"
+        descriptionLabel.text = "description"
+        imageView.image = UIImage(systemName: "applelogo")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension TodayCollectionViewCell {
+    func setupLayout() {
+        [
+            imageView,
+            subTitleLabel,
+            titleLabel,
+            descriptionLabel,
+            
+        ].forEach { addSubview($0) }
+        
+        subTitleLabel.snp.makeConstraints { make in
+            make.leading.top.trailing.equalToSuperview().inset(24.0)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(subTitleLabel.snp.leading)
+            make.trailing.equalTo(subTitleLabel.snp.trailing)
+            make.top.equalTo(subTitleLabel.snp.bottom).offset(4.0)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview().inset(24.0)
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
--- MyAppStore/Scene/Today/TodayViewController.swift
+++ MyAppStore/Scene/Today/TodayViewController.swift
@@ -6,11 +6,56 @@
 //
 
 import UIKit
+import SnapKit
 
 class TodayViewController: UIViewController {
+    
+    private lazy var collectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.register(TodayCollectionViewCell.self, forCellWithReuseIdentifier: "todayCell")
+        
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        
+    }
+}
+
+extension TodayViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "todayCell", for: indexPath)
+        
+        cell.backgroundColor = .darkGray
+        
+        return cell
+    }
+    
+    
+}
+
+extension TodayViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = collectionView.frame.width - 32.0
+        
+        return CGSize(width: width, height: width)
     }
 }
