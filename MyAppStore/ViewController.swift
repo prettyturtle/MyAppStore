83ebc02368e0d2206925f1b627026a9300b28789
prettyturtle <unifconti@gmail.com>
2021-12-17 23:55:04 +0900

    Initial Commit

---
+++ MyAppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAppStore
+//
+//  Created by yc on 2021/12/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
